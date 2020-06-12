e3939e9120f46a075b34e78aa0fd69ccbeae3209
Jeff Araujo <jefferson_lord@yahoo.com.br>
2020-06-12 01:07:44 -0300

    atualizando meu pokeapi

--- pokemon/js/scriptPoke.js
+++ pokemon/js/scriptPoke.js
@@ -3,6 +3,58 @@ const capitalize = (s) => {
     return s.charAt(0).toUpperCase() + s.slice(1)
 }
 
+function compare(a, b) {
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  }
+  // objs.sort(compare);
+
+  function verificaCaixaDeTexto(){
+    if(  document.getElementById("caixaDeTexto").value == false){
+      alert("Insira um pokemon")
+    }
+  }
+
+  function organizaListagem() {
+    listaPokemon = listaPokemonDesordenada.sort(compare)
+    console.log(listaPokemon)
+  }
+
+//   function reset() {
+//     let resetar = document.getElementById('caixaDeTexto'); // Seleciona o formulário que deseja limpar os campos
+//     formulario.reset(); // Executa a ação de limpar todos os campos
+//   }
+
+  let listaNomesPokemon = []
+let listaPokemonDesordenada = []
+let listaPokemon = []
+
+function geraListagem() {
+    axios.get("https://pokeapi.co/api/v2/pokemon/?limit=807")
+      .then((resposta) => {
+        retorno = resposta.data.results
+        retorno.forEach((indice) => { listaNomesPokemon.push(indice) })
+        listaNomesPokemon.forEach((indice) => {
+          axios.get(`https://pokeapi.co/api/v2/pokemon/${indice.name}`)
+            .then((resposta) => {
+              const pokemon = resposta.data
+              var numero = pokemon.id.toString()
+              if (numero.length == 1) { numero = `00${numero}` }
+              if (numero.length == 2) { numero = `0${numero}` }
+              var sprite = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${numero}.png`
+              indice.img = sprite
+              var insercao = { name: capitalize(indice.name), img: indice.img, num: numero }
+              listaPokemonDesordenada.push(insercao)
+            })
+        })
+      })
+  }
+
 async function nomeIdDoPokemon() {
     var caixaDeTexto = document.getElementById('caixaDeTexto')
     var pesquisa = caixaDeTexto.value.toLowerCase()
@@ -41,6 +93,48 @@ axios.get(`https://pokeapi.co/api/v2/pokemon/${pesquisa}`)
         )
 }
 
-
+$(function () {
+    listaPokemon;
+    $("#caixaDeTexto").autocomplete({
+      minLength: 1,
+      source: function (request, response) {
+        response($.map(listaPokemon, function (obj, key) {
+  
+          var name = obj.name.toUpperCase();
+  
+          if (name.indexOf(request.term.toUpperCase()) != -1) {
+            return {
+              label: obj.name, // Label for Display
+              // value: obj.id // Value
+              img: obj.img,
+              num: obj.num
+            }
+          } else {
+            return null;
+          }
+        }));
+      },
+      focus: function (event, ui) {
+        event.preventDefault();
+      },
+      // Once a value in the drop down list is selected, do the following:
+      select: function (event, ui) {
+        event.preventDefault();
+        // place the person.given_name value into the textfield called 'select_origin'...
+        $('#caixaDeTexto').val(ui.item.label);
+        nomeIdDoPokemon()
+        // ... any other tasks (like setting Hidden Fields) go here...
+      },
+      html: true,
+      open: function (event, ui) {
+        $(".ui-autocomplete").css("z-index", 1000);
+  
+      }
+    })
+      .autocomplete("instance")._renderItem = function (ul, item) {
+        // return $("<li><div><img src=" + item.img + "><span>" + item.value + "</span></div></li>").appendTo(ul);
+        return $(`<li><div><img src=${item.img}><span> N° ${item.num} - </span><span>${item.value}</span></div></li>`).appendTo(ul);
+      };
+  })
 
 
